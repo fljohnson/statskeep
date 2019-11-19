8a2075d6c181aabf80ca5dab70bbecd3efc994a0
Floyd Johnson <fljohnson3@isp.com>
2019-11-19 14:33:27 -0500

    Trying it as create on first run

--- DB.js
+++ DB.js
@@ -1,25 +1,13 @@
 
 import SQLite from 'react-native-sqlite-storage';
-
+import Alert  from 'react-native';
 const database_name = "lemonwhiz.db";
 const database_version = "1.0";
 const database_displayname = "SQLite Test Database";
 const database_size = 200000;
 
 function openCB() {
-  console.log("Database OPENED");
-}
-function errorCB(err) {
-  Alert.alert("FALTA!",JSON.stringify(err));
-}	
-export class Database {
-	//static db = SQLite.openDatabase({name : "testDB", createFromLocation : "~data/mydbfile.sqlite"}B);
-	static db = openDatabase({ name: 'lemon_db.db', createFromLocation : '~lemon_db.db', location:'Library'}, openCB,errorCB);
-	/*
-	static db = SQLite.openDatabase(database_name, database_version, database_displayname, database_size,this.openCB,this.errorCB); 
-
-				  () => {
-					  db.transaction((tx) => {
+  Database.db.transaction((tx) => {
 							tx.executeSql(
 							"CREATE TABLE `stats` ("+
 							"`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"+
@@ -42,16 +30,15 @@ export class Database {
 							"`active`	DESC"+
 						")"
 							)	;
-						},
-						error => {
-									Alert.alert("Bombed on create:",JSON.stringify(error));
-								  }
-						
-						);
-					  },
-				  error => {
-					Alert.alert("Different crash on create:",JSON.stringify(error));
-				  });
-				  */
+						}
+}
+function errorCB(err) {
+  Alert.alert("FALTA!",JSON.stringify(err));
+}	
+export class Database {
+	//static db = SQLite.openDatabase({name : "testDB", createFromLocation : "~data/mydbfile.sqlite"}B);
+	static db = SQLite.openDatabase(database_name, database_version, database_displayname, database_size,openCB,errorCB); 
+
+
 	
 }
