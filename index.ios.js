67f98640281124c535321eb5f29b30ed2fbe8424
Floyd Johnson <fljohnson3@isp.com>
2019-11-24 15:12:46 -0500

    blast! I (expletive) forgot to bring in the def for ThemeProvider. This compiler sucks

--- index.ios.js
+++ index.ios.js
@@ -3,6 +3,7 @@
  */
 import * as React from 'react';
 import {AppRegistry} from 'react-native';
+import { ThemeProvider } from 'react-native-ios-kit';
 import App from './App';
 import {name as appName} from './app.json';
 
